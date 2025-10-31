1b0abece591ea26fa29fcb06091372a6fbbb9bc3
Andrei137 <andrei.fabian188@gmail.com>
2025-10-31 13:52:14 +0200

    Update wezterm

--- dotfiles/.wezterm/src/core/startup.lua
+++ dotfiles/.wezterm/src/core/startup.lua
@@ -3,15 +3,21 @@ local M = {}
 function M.apply(wezterm, config)
 	wezterm.on("gui-startup", function()
 		local screen = wezterm.gui.screens().active
-		local tab, pane, window = wezterm.mux.spawn_window({
-			position = {
-				x = screen.x,
-				y = screen.y,
-				origin = "ActiveScreen",
-			},
-		})
+		local tab, pane, window = wezterm.mux.spawn_window({})
 		local gui_window = window:gui_window()
-		gui_window:toggle_fullscreen()
+
+		gui_window:set_inner_size(screen.width * 0.975, screen.height * 0.925)
+
+		wezterm.sleep_ms(10)
+
+		local dims = gui_window:get_dimensions()
+		local outer_width = dims.pixel_width
+		local outer_height = dims.pixel_height
+
+		local x = (screen.width - outer_width) / 2
+		local y = (screen.height - outer_height) / 2
+
+		gui_window:set_position(x, y)
 	end)
 
 	-- Domains
